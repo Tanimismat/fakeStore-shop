aed28b62c025150b9c2e6bb0a92af5a3e55c59c3
Tanimismat <tanim.ismat@gmail.com>
2022-04-09 16:22:28 +0600

    image sized

--- src/Components/SingleProduct.js
+++ src/Components/SingleProduct.js
@@ -6,7 +6,7 @@ const SingleProduct = ({ product, setCartCount }) => {
     return (
         <div data-aos="flip-left" className='col-md-4'>
             <div className="card p-3 ">
-                <img className='w-25 m-auto' src={product.image} alt="" />
+                <img className='m-auto image' src={product.image} alt="" />
                 <h5>{product.title.slice(0, 10)}</h5>
                 <div className="d-flex justify-content-around">
                     <button className='btn btn-success' onClick={setCartCount}>Add to cart</button>
